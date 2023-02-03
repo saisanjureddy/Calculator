65e6222f8f4751e259eaa564e0d734116f444971
saisanjureddy <99756816+saisanjureddy@users.noreply.github.com>
2023-02-02 20:05:33 -0500

    appIcon and code alteration

--- Calculator/ViewController.swift
+++ Calculator/ViewController.swift
@@ -1,4 +1,3 @@
-//
 //  ViewController.swift
 //  Calculator
 //
@@ -7,133 +6,180 @@
 
 import UIKit
 import AVKit
-import AVFoundation
+import AVFoundation /**/
 
 class ViewController: UIViewController {
-//    var audioPlayer = AVAudioPlayer()
+    //audio player property to retain when playing sound required
     var player: AVAudioPlayer?
 
-    //MARK: - The view controller controls the view
-    //MARK: - IBOulet - reference a UI Element
-    @IBOutlet var outputLbl: UILabel! //screen
+    // MARK: - The View Controller Controls The View.
+    // MARK: - IBOulet - Reference A UI Element.
     
-    //Properties
-    var numberOnScreen: Double = 0 //current number on screen in label
+    // Display Screen:
     
-    var previousNumber: Double = 0 //the previously entered number
+    @IBOutlet var outputLbl: UILabel!
     
-    var performingMath = false //this will check if operators have been selected
+    // Properties
     
-    var operation = 0 //this will know what operation is being pressed
+    // Variable For Current Number On Screen In Label:
+    var numberOnScreen: Double = 0
     
-    //A number will represent each operation
-    var btnSound: AVAudioPlayer! //To play a sound when button is clicked
+    // Variable For The Previously Entered Number:
+    var previousNumber: Double = 0
     
-    override func viewDidLoad() {
+    // This Variable Will Check If Operators Have Been Selected:
+    var performingMath = false
+    
+    // This Variable Will Know What Operation Is Being Pressed:
+    var operation = 0    // A Number Will Represent Each Operation
+    
+    // Variable To Play A Sound When Button Is Clicked:
+    var btnSound: AVAudioPlayer!
+    
+    override func viewDidLoad()
+    {
+        // Do Any Additional Setup After Loading The View.
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
     
-    //MARK: - IBAction - perform an action on a button
-
-    @IBAction func numberBtn(_ sender: UIButton) {
-        //When a number button is pressed
+    // MARK: - IBAction - Perform An Action On A Button.
+    
+    // When A Number Button Is Pressed:
+    @IBAction func numberBtn(_ sender: UIButton)
+    {
+        // To Play Sound:
         playSound()
-        if performingMath == true {
-            //If we have pressed an operator
-            outputLbl.text = "" //clear the label on calculator
-            outputLbl.text = String(sender.tag) //pass in the number of the button pressed
-            numberOnScreen = Double(outputLbl.text!)! //need to set numberOnScreen to whatever the output label is
+        
+        if performingMath == true
+        {
+            // If We Pressed An Operator:
+            outputLbl.text = ""                             // Clear The Label On Calculator.
+            outputLbl.text = String(sender.tag)             // Pass In The Number Of The Button Pressed.
+            numberOnScreen = Double(outputLbl.text!)!       // Need To Set NumberOnScreen To Whatever The Output Label Is.
             performingMath = false
-        } else {
-            //If we have NOT pressed an operator
-            outputLbl.text = outputLbl.text! + String(sender.tag) //append whatever is pressed and output to the label
-            numberOnScreen = Double(outputLbl.text!)! //need to set numberOnScreen to whatever the output label is
+        }
+        else
+        {
+            //If We Have NOT Pressed An Operator:
+            outputLbl.text = outputLbl.text! + String(sender.tag)  // Append Whatever Is Pressed And Output To The Label.
+            numberOnScreen = Double(outputLbl.text!)!              // Need To Set NumberOnScreen To Whatever The Output Label Is.
         }
     }
     
-    @IBAction func operators(_ sender: UIButton) {
-        //When an operator is pressed
+    // When An Operator Is Pressed:
+    @IBAction func operators(_ sender: UIButton)
+    {
+        // To Play Sound:
         playSound()
+        
         //10 = C
         //11 = EQUALS
         //12 = ADD
         //13 = SUBSTRACT
         //14 = DIV
         //15 = MUL
-        //16 = Percent
+        //16 = PERCENTAGE
         
         if(outputLbl.text != "" && sender.tag != 11 && sender.tag != 10) {
-            //If we have not pressed equals or clear, and the output label is not empty
+            // If We Have Not Pressed Equals Or Clear The Output Label Is Not Empty.
+            
             previousNumber = Double(outputLbl.text!)!
             
-            //Operations
-            if sender.tag == 12 {
-                //Add
+            // Operations:
+            
+            if sender.tag == 12
+            {
+                // ADD
                 outputLbl.text = "+"
-            } else if sender.tag == 13 {
-                //subtract
+            }
+            else if sender.tag == 13
+            {
+                // SUBSTRACT
                 outputLbl.text = "-"
-            } else if sender.tag == 14 {
-                //div
+            }
+            else if sender.tag == 14
+            {
+                // DIV
                 outputLbl.text = "/"
-            } else if sender.tag == 15 {
-                //mul
+            }
+            else if sender.tag == 15
+            {
+                // MUL
                 outputLbl.text = "*"
             }
-            
-            operation = sender.tag //we will know which operator has been pressed
-            
-            performingMath = true //we know we have to do math now
+            // We Will Know Which Operator Has Been Pressed.
+            operation = sender.tag
+            // We Have To Perform Math Now.
+            performingMath = true
         } else if sender.tag == 11 {
-            //pressed equals
-            
-            if operation == 12 {
-                //adding
+            // Pressed Equals:
+            if operation == 12
+            {
+                // Adding -
                 outputLbl.text = String(previousNumber + numberOnScreen)
-            } else if operation == 13 {
-                //sub
+            }
+            else if operation == 13
+            {
+                // Subtarcting -
                 outputLbl.text = String(previousNumber - numberOnScreen)
-            } else if operation == 14 {
-                //dividing
+            }
+            else if operation == 14
+            {
+                // Dividing -
                 outputLbl.text = String(previousNumber / numberOnScreen)
-            } else if operation == 15 {
-                //multiplying
+            }
+            else if operation == 15
+            {
+                // Multiplying -
                 outputLbl.text = String(previousNumber * numberOnScreen)
-            } else if operation == 16 {
+            }
+            else if operation == 16
+            {
+                // Finding Percentage -
                 outputLbl.text = String(calculatePercentage(value: previousNumber, percentageVal: numberOnScreen))
             }
             
-        } else if sender.tag == 10 {
-            //pressed clear
-            outputLbl.text = "" //clear the output
-            previousNumber = 0 //reset
-            numberOnScreen = 0 //reset
+        }
+        
+        
+        else if sender.tag == 10
+        {
+            // Pressed Clear:
+            outputLbl.text = ""     // Clear The Output
+            previousNumber = 0      // Reset
+            numberOnScreen = 0      // Reset
         }
         
     }
     
-    func playSound() {
-        guard let url = Bundle.main.url(forResource: "mixkit-single-classic-click-1116", withExtension: "wav") else { return }
-
-        do {
-            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
-            try AVAudioSession.sharedInstance().setActive(true)
-            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
-
-            guard let player = player else { return }
-
-            player.play()
-
-        } catch let error {
-            print(error.localizedDescription)
-        }
-    }
-    
-    public func calculatePercentage(value:Double,percentageVal:Double)->Double{
+    // Function For Percentage:
+    public func calculatePercentage(value:Double,percentageVal:Double)->Double
+    {
         let val = value * percentageVal
         return val / 100.0
     }
+    
+    // Function To Play Sound:
+    func playSound()
+    {
+        //Sound source path/url and used to initialize in AVAudioPlayer
+        guard let url = Bundle.main.url(forResource: "mixkit-single-classic-click-1116", withExtension: "wav") else { return }
 
+        //error handling
+        do
+        {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.mp3.rawValue)
+
+            //unwraping optionlal value
+            guard let player = player else { return }
+            player.play()
+        }
+        catch let error
+        {
+            print(error.localizedDescription)
+        }
+    }
 }
-
